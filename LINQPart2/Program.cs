15020c239a4ef01b5f4db8ae097c5cf3ef361403
Dmitry <grammattika@gmail.com>
2023-07-03 18:23:55 +0300

    Добавьте файлы проекта.

---
+++ LINQPart2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LINQPart2{
+    internal class Program{
+        static void Main(string[] args){
+
+            //Задание 15.1.4 Напишите метод для поиска общих букв в двух словах.
+
+            string[] WO = { "Тебе" };
+            string[] GO = { "Тебе" };
+
+            var DO = WO.Intersect(GO);
+
+            foreach(var x in DO) { Console.WriteLine(x); }
+        }
+    }
+}
