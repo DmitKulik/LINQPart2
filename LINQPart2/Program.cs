606c8d837e011371e11364b7a9ec428f959eaa8a
Dmitry <grammattika@gmail.com>
2023-07-03 18:30:36 +0300

    ответ список содержащихся в строке уникальных символов без пробелов и следующих знаков препинания

--- LINQPart2/Program.cs
+++ LINQPart2/Program.cs
@@ -2,14 +2,29 @@
     internal class Program{
         static void Main(string[] args)
         {
-            Console.WriteLine(CountCommon("one", "two"));
-        }
+            Console.WriteLine("Введите текст:");
 
-        static int CountCommon(string word1, string word2)
-        {
-            var amount = word1.Intersect(word2)//   ищем пересечение
-     .Count(); // считаем количество
-            return amount;
+            // читаем ввод
+            var text = Console.ReadLine();
+
+            // сохраняем список знаков препинания и символ пробела в коллекцию
+            var punctuation = new List<char>() { ' ', ',', '.', ';', ':', '!', '?' };
+
+            // валидация ввода
+            if (string.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("Вы ввели пустой текст");
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Текст без знаков препинания: ");
+
+            // так как строка - это массив char, мы можем вызвать метод  except  и удалить знаки препинания
+            var noPunctuation = text.Except(punctuation).ToArray();
+
+            // вывод
+            Console.WriteLine(noPunctuation);
         }
     }
 }
